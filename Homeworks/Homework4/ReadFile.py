db7f265b877845e48511db8ff4f7ae965e9afe32
farahhfauzi <44912016+farahhfauzi@users.noreply.github.com>
2020-02-08 20:57:03 -0700

    Add files via upload

---
+++ Homeworks/Homework4/ReadFile.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+'''
+Re-do of Homework2
+Farah Fauzi
+'''
+
+import numpy as np
+import astropy.units as u
+
+def Read(filename):
+    file = open(filename,'r')
+ 
+    line1 = file.readline()                 #read the first line in MW_000.txt file
+    label, value = line1.split()
+    time = float(value)*u.Myr               #store the line read as time
+
+    line2 = file.readline()                 #read the 2nd line in MW_000,txt file
+    label, value = line2.split()
+    total = float(value)                    #store the line read as total
+
+    file.close()                            #close file
+
+    data = np.genfromtxt(filename,dtype=None,names=True,skip_header=3) #store remainding data, skip first 3 rows
+
+    #print (data['type'][1])                 #test print out particle type of 2nd particle
+
+    return time, total, data
+
+Read("MW_000.txt")
+
